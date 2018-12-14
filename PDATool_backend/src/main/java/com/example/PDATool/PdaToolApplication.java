d2669412565498738ed4482dbd4e679267d9dbbe
Graham-Stein <graham@guavoaviation.co.uk>
2018-12-14 14:21:47 +0000

    Add initial commit

---
+++ PDATool_backend/src/main/java/com/example/PDATool/PdaToolApplication.java
@@ -0,0 +1,14 @@
+package com.example.PDATool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdaToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdaToolApplication.class, args);
+	}
+
+}
+
