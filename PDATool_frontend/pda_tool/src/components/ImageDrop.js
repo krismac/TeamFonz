26815425dd262c9761925470c783650600d4422f
krismac <email@krismac.com>
2018-12-20 14:14:42 +0000

    additions for presentation

--- PDATool_frontend/pda_tool/src/components/ImageDrop.js
+++ PDATool_frontend/pda_tool/src/components/ImageDrop.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
+import CloudKey from './helpers/keys.js';
 
-
-const CLOUDINARY_UPLOAD_PRESET = 'l67ebvpg';
+const CLOUDINARY_UPLOAD_PRESET = CloudKey;
 const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/fonz/upload';
 
 export default class ImageDrop extends React.Component {
